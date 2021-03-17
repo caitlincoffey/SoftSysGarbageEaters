2c0b709df0746f2e9aae3f3b8c542529893baf72
GatiAher <gativaher@gmail.com>
2021-03-16 23:53:32 -0400

    Add ADD opcode and restructure file order to follow C best practices

--- main.c
+++ main.c
@@ -9,12 +9,21 @@
 #include <stdint.h>
 #include <stdio.h>
 
+/**
+ *  static memory:
+ * 2^16 memory locations of 16 bits each.
+ * Addresses are numbered from 0 (x0000) to 65,535 (xFFFF)
+ */
+uint16_t memory[65535];
+
 // TODO: function headers
 // TODO: TRAP functions (are we writing those in C or assembly?)
 // TODO: other input handling functions?
 // TODO: functions for terminal interfacing?
 // TODO: functions for keyboard interfacing?
 uint16_t get_sign_extension(uint16_t n, int num_bits);
+void update_flag(uint16_t value);
+
 uint16_t read_from_memory(uint16_t address);
 void write_to_memory(uint16_t address, uint16_t value);
 int read_program_code_into_memory(const char *path_to_code);
@@ -24,57 +33,9 @@ int read_program_code_into_memory(const char *path_to_code);
  * and then we join them together with pipelining in the main functions?
  * I still need to figure out what exactly that entails*/
 /* each function gets an instruction with the first four bits (opcode) removed */
-uint16_t op_add(uint16_t bits);
-
-/**
- *  static memory:
- * 2^16 memory locations of 16 bits each.
- * Addresses are numbered from 0 (x0000) to 65,535 (xFFFF)
- */
-uint16_t memory[65535];
-
-uint16_t read_from_memory(uint16_t address)
-{
-
-  // if (address == kbsr) { // placeholder
-  //   // we have some key -> hashing
-  //   // hashing
-  //   // translating address to something usable
-  // }
-
-  return memory[address];
-}
-
-void write_to_memory(uint16_t address, uint16_t value)
-{
-  memory[address] = value;
-}
-
-int read_program_code_into_memory(const char *path_to_code)
-{
-  FILE *code_file = fopen(path_to_code, "r");
-
-  /* Return error if we cannot read file */
-  if (!code_file)
-  {
-    fprintf(stderr, "Error: Could not find file %s\n", path_to_code);
-    return 0;
-  }
-
-  /* The first 16 bits of the program file
-  specify the address in memory where the program should start. */
-  uint16_t program_start;
-  fread(&program_start, 16, 1, code_file);
-
-  /* reading once because we are reading the entire file */
-  uint16_t max_space = 65535 - program_start; // 2^16 - program_start
-  uint16_t *point_to_mem = memory + program_start;
-  unsigned long read_to_mem = fread(point_to_mem, 16, max_space, code_file);
-  // idk how much this needs but so I'll give it a long
-
-  fclose(code_file);
-  return 1;
-}
+void op_add(uint16_t bits);
+void op_and(uint16_t bits);
+void op_not(uint16_t bits);
 
 /* ENUMS */
 
@@ -121,9 +82,9 @@ enum instruction_set
 /* condition flags: three 1-bit registers: N (negative) Z (zero) P (positive) */
 enum cond_flag
 {
-  N = 0b100,
-  Z = 0b010,
-  P = 0b001
+  F_N = 0b100,
+  F_Z = 0b010,
+  F_P = 0b001
 };
 
 /* trap codes: 6 trap code operations */
@@ -146,6 +107,99 @@ enum mem_registers
   M_DDR = 0xFE06,  // display data register
   M_MCR = 0xFFFE   // machine control register
 };
+
+/* General Helper Functions */
+
+void update_flag(uint16_t value)
+/*
+  Update the condition flag register depending on the passed in value.
+*/
+{
+  if (value == 0)
+  {
+    reg[R_F] = F_Z;
+  }
+  else if ((value >> 15 & 0x1) == 1)
+  {
+    reg[R_F] = F_N;
+  }
+  else
+  {
+    reg[R_F] = F_P;
+  }
+}
+
+uint16_t read_from_memory(uint16_t address)
+{
+
+  // if (address == kbsr) { // placeholder
+  //   // we have some key -> hashing
+  //   // hashing
+  //   // translating address to something usable
+  // }
+
+  return memory[address];
+}
+
+void write_to_memory(uint16_t address, uint16_t value)
+{
+  memory[address] = value;
+}
+
+int read_program_code_into_memory(const char *path_to_code)
+{
+  FILE *code_file = fopen(path_to_code, "r");
+
+  /* Return error if we cannot read file */
+  if (!code_file)
+  {
+    fprintf(stderr, "Error: Could not find file %s\n", path_to_code);
+    return 0;
+  }
+
+  /* The first 16 bits of the program file
+  specify the address in memory where the program should start. */
+  uint16_t program_start;
+  fread(&program_start, 16, 1, code_file);
+
+  /* reading once because we are reading the entire file */
+  uint16_t max_space = 65535 - program_start; // 2^16 - program_start
+  uint16_t *point_to_mem = memory + program_start;
+  unsigned long read_to_mem = fread(point_to_mem, 16, max_space, code_file);
+  // idk how much this needs but so I'll give it a long
+
+  fclose(code_file);
+  return 1;
+}
+
+/* Opcode Functions */
+
+/* Note: to break bits into sections
+*  - use bit-shift to erase bits right of target section
+*  - use mask of 1s of size of target section to only keep bits of target section (remove bits left of target section)
+*/
+
+void op_add(uint16_t bits)
+{
+  /* destination register (DR) */
+  uint16_t DR = (bits >> 9) & 0x7;
+  /* first source register (SR1) */
+  uint16_t SR1 = (bits >> 6) & 0x7;
+  /* check flag source register for mode of ADD */
+  if ((bits >> 5) & 0x1)
+  {
+    /* mode 1: add a small constant */
+    uint16_t imm5 = get_sign_extension(bits & 0x1, 5);
+    reg[DR] = reg[SR1] + imm5;
+  }
+  else
+  {
+    /* mode 2: add value from second source register */
+    uint16_t SR2 = (bits >> 0);
+    reg[DR] = reg[SR1] + reg[SR2];
+  }
+  update_flag(reg[DR]);
+}
 
 /* priority level, processor status register, privilege mode, privilege mode exception? */
 /* QUESTION: do we need to implement operating system and supervisor stack: memory adresses x0200 through x2FFF */
