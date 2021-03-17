b290f8bb83ca5e9e3382ed11a178f2bb3c1105f7
GatiAher <gativaher@gmail.com>
2021-03-16 21:23:57 -0400

    Add import, changed enum vars to all caps

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 
 /* includes */
 #include <stdint.h>
+#include <stdio.h>
 
 // TODO: function headers
 // TODO: TRAP functions (are we writing those in C or assembly?)
@@ -16,7 +17,7 @@
 uint16_t get_sign_extension(uint16_t n, int num_bits);
 uint16_t read_from_memory(uint16_t address);
 void write_to_memory(uint16_t address, uint16_t value);
-int read_program_code_into_memory(const char path_to_code);
+int read_program_code_into_memory(const char *path_to_code);
 
 /* instruction set function definitions */
 /** NOTE: I think we should divide and conquer to write them all
@@ -32,29 +33,33 @@ uint16_t op_add(uint16_t bits);
  */
 uint16_t memory[65535];
 
-uint16_t read_from_memory(uint16_t address) {
+uint16_t read_from_memory(uint16_t address)
+{
 
-    // if (address == kbsr) { // placeholder
-    //   // we have some key -> hashing
-    //   // hashing
-    //   // translating address to something usable
-    // }
+  // if (address == kbsr) { // placeholder
+  //   // we have some key -> hashing
+  //   // hashing
+  //   // translating address to something usable
+  // }
 
   return memory[address];
 }
 
-void write_to_memory(uint16_t address, uint16_t value){
+void write_to_memory(uint16_t address, uint16_t value)
+{
   memory[address] = value;
 }
 
-int read_program_code_into_memory(const char *path_to_code) {
-  FILE* code_file = fopen(path_to_code, "r");
+int read_program_code_into_memory(const char *path_to_code)
+{
+  FILE *code_file = fopen(path_to_code, "r");
 
   /* Return error if we cannot read file */
-  if (!code_file) {
-    fprintf(stderr, "Error: Could not find file %s\n", path_to_code)
+  if (!code_file)
+  {
+    fprintf(stderr, "Error: Could not find file %s\n", path_to_code);
     return 0;
-  };
+  }
 
   /* The first 16 bits of the program file
   specify the address in memory where the program should start. */
@@ -69,30 +74,50 @@ int read_program_code_into_memory(const char *path_to_code) {
 
   fclose(code_file);
   return 1;
-
 }
 
 /* ENUMS */
 
 /* registers: 8 general, 1 program counter (PC), 1 condition register */
 
-enum registers {
-r0=0,
-r1,
-r2,
-r3,
-r4,
-r5,
-r6,
-r7,
-rpc,
-rf,
-rsize
+enum registers
+{
+  R_0 = 0,
+  R_1,
+  R_2,
+  R_3,
+  R_4,
+  R_5,
+  R_6,
+  R_7,
+  R_PC,
+  R_F,
+  R_SIZE
 };
 
-uint16_t reg[rsize];
+uint16_t reg[R_SIZE];
 
 /* intruction set: 14 instructions, 1 reserved, 1 unused */
+/* can use */
+enum instruction_set
+{
+  OP_BR = 0, /* branch */
+  OP_ADD,    /* add  */
+  OP_LD,     /* load */
+  OP_ST,     /* store */
+  OP_JSR,    /* jump register */
+  OP_AND,    /* bitwise and */
+  OP_LDR,    /* load register */
+  OP_STR,    /* store register */
+  OP_RTI,    /* unused */
+  OP_NOT,    /* bitwise not */
+  OP_LDI,    /* load indirect */
+  OP_STI,    /* store indirect */
+  OP_JMP,    /* jump */
+  OP_RES,    /* reserved (unused) */
+  OP_LEA,    /* load effective address */
+  OP_TRAP    /* execute trap */
+};
 
 /* condition codes: three 1-bit registers: N (negative) Z (zero) P (positive) */
 
@@ -100,12 +125,13 @@ uint16_t reg[rsize];
 
 /* memory-mapped I/O: memory addresses xFE00 through xFFFF have been allocated to designate each I/O device register. */
 
-enum mem_registers{
-  kbsr = 0xFE00, // keyboard status register
-  kbdr = 0xFE02, // keyboard data register
-  dsr = 0xFE04, // display status register
-  ddr = 0xFE06, // display data register
-  mcr = 0xFFFE // machine control register
+enum mem_registers
+{
+  M_KBSR = 0xFE00, // keyboard status register
+  M_KBDR = 0xFE02, // keyboard data register
+  M_DSR = 0xFE04,  // display status register
+  M_DDR = 0xFE06,  // display data register
+  M_MCR = 0xFFFE   // machine control register
 };
 
 /* priority level, processor status register, privilege mode, privilege mode exception? */
@@ -114,56 +140,57 @@ enum mem_registers{
 
 int main(int argc, const char *argv[])
 {
-    // NOTE: if anything goes wrong, change this status
-    int status = 1;
+  // NOTE: if anything goes wrong, change this status
+  int status = 1;
 
-    if (argc <= 1) {
-       fprintf(stderr, "Error: No input files\n");
-       return 0;
-   }
+  if (argc <= 1)
+  {
+    fprintf(stderr, "Error: No input files\n");
+    return 0;
+  }
 
-    // Filename or path to file of our program we want to run
-    char *path_to_code = argv[1];
+  // Filename or path to file of our program we want to run
+  char *path_to_code = argv[1];
 
-    read_program_code_into_memory(path_to_code);
+  read_program_code_into_memory(path_to_code);
 
-    /* 0x3000 is the default PC position, start of memory available for user programs. */
-    int PC_INIT = 0x3000;
+  /* 0x3000 is the default PC position, start of memory available for user programs. */
+  int PC_INIT = 0x3000;
 
-    while (1)
-    {
-        // Follow Classic RISC Pipeline
-        /**
+  while (1)
+  {
+    // Follow Classic RISC Pipeline
+    /**
          * 1. IF. instruction fetch
          * load one instruction from memory at the address of PC register,
          * increment PC register by 4,
          * and choose whether to take that as next PC or to take result of a branch / jump calculation as the next PC
          * */
-        uint16_t opcode;
+    uint16_t opcode;
 
-        /**
+    /**
          * 2. ID. instruction decode and register fetch
          * look at opcode to determine which type of instruction it should perform and on what values,
          * if instruction is a branch or jump, the target address of the branch or jump is computed in parallel with reading the register file
          * */
-        switch (opcode)
-        {
-        case 1:
-            // NOTE: I think 3 and 4 and specific to the opcode and should happen in the function
-            // QUESTION: is 5 going to be the same for all the opcodes?
-            /* 3. EX. execute */
-            /* 4. MEM. memory access */
-            /** 5. WB. register write back
+    switch (opcode)
+    {
+    case 1:
+      // NOTE: I think 3 and 4 and specific to the opcode and should happen in the function
+      // QUESTION: is 5 going to be the same for all the opcodes?
+      /* 3. EX. execute */
+      /* 4. MEM. memory access */
+      /** 5. WB. register write back
              * update general registers with output
              * update COND register with N Z P value
              * */
-            op_add(1);
-            break;
+      op_add(1);
+      break;
 
-        default:
-            break;
-        }
+    default:
+      break;
     }
+  }
 
-    return status;
+  return status;
 }
