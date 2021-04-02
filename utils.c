ac55e49cefae55c995baf9adf1a280f4b986d428
Mickmcginnis <zoekauai@gmail.com>
2021-04-01 21:00:12 -0400

    implemented unix-specific terminal things

--- utils.c
+++ utils.c
@@ -104,3 +104,38 @@ int read_program_code_into_memory(const char *path_to_code)
   fclose(code_file);
   return 1;
 }
+
+uint16_t check_key()
+{
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
+    return select(1, &readfds, NULL, NULL, &timeout) != 0;
+}
+
+struct termios original_tio;
+
+void disable_input_buffering()
+{
+    tcgetattr(STDIN_FILENO, &original_tio);
+    struct termios new_tio = original_tio;
+    new_tio.c_lflag &= ~ICANON & ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+}
+
+void restore_input_buffering()
+{
+    tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+}
+
+void handle_interrupt(int signal)
+{
+    restore_input_buffering();
+    printf("\n");
+    exit(-2);
+}
+
--- utils.h
+++ utils.h
@@ -1,6 +1,11 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
+#include <string.h>
+#include <stdio.h>
+#include <sys/termios.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include "opcode.h"
 
 extern uint16_t memory[65535];
@@ -37,5 +42,9 @@ void update_flag(uint16_t value);
 uint16_t read_from_memory(uint16_t address);
 void write_to_memory(uint16_t address, uint16_t value);
 int read_program_code_into_memory(const char *path_to_code);
+
+uint16_t check_key();
+
+void handle_interrupt(int signal);
 
 #endif
