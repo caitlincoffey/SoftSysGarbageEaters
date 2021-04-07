597803fd6b171e22017aea15a2c98b193a088357
Mickmcginnis <zoekauai@gmail.com>
2021-04-06 23:28:23 -0400

    commented main and opcode files

--- main.c
+++ main.c
@@ -2,26 +2,27 @@
  * Garbage Eaters: LC-3 Virtual Machine
  *
  * Main loop follows Classic RISC Pipeline
- * 1. IF. instruction fetch
- *  load one instruction from memory at the address of PC register,
- *  increment PC register by 4,
- *  and choose whether to take that as next PC or to take result of a branch / jump calculation as the next PC
+ *  1. IF. instruction fetch
+ *    Load one instruction from memory at the address of PC register, increment
+ *    PC register by 4, and choose whether to take that as next PC or to take 
+ *    result of a branch / jump calculation as the next PC
  *
- * 2. ID. instruction decode and register fetch
- * look at opcode to determine which type of instruction it should perform and on what values,
- * if instruction is a branch or jump, the target address of the branch or jump is computed in parallel with reading the register file
+ *  2. ID. instruction decode and register fetch
+ *    Look at opcode to determine which type of instruction it should perform 
+ *    and on what values; if instruction is branch or jump, the target address 
+ *    of the branch or jump is computed in parallel with reading register file 
  *
- * 3. EX. execute
+ *  3. EX. execute
  *
- * 4. MEM. memory access
+ *  4. MEM. memory access
  *
- * 5. WB. register write back
- *  update general registers with output
- *  update COND register with N Z P value
+ *  5. WB. register write back
+ *    Update general registers with the output; update condition register with 
+ *    N Z P values
  *
  * Date: 03/06/2021
  * Authors: Gati Aher, Navi Sai, Caitlin Coffey, Zoe McGinnis
- **/
+ */
 
 /* includes */
 #include <stdint.h>
@@ -39,27 +40,13 @@
 #include "opcode.h"
 #include "utils.h"
 
-// void printb(int n)
-// {
-//   while (n)
-//   {
-//     if (n & 1)
-//       printf("1");
-//     else
-//       printf("0");
-//
-//     n >>= 1;
-//   }
-//   printf("\n");
-// }
-
 extern int errno;
 
 int main(int argc, const char *argv[])
 {
+  // use errno for error handling
   int errnum;
-  if (argc <= 1)
-  {
+  if (argc <= 1) {
     errno = 2;
     errnum = errno;
     fprintf(stderr, "Value of errno:%d\n", errno);
@@ -70,75 +57,105 @@ int main(int argc, const char *argv[])
 
   // make it work with unix terminal
   signal(SIGINT, handle_interrupt);
-  // signal(SIGABRT | SIGINT, handle_interrupt);
   disable_input_buffering();
 
-  // File path to to program LC-3 should run
+  // file path to program LC-3 should run
   const char *path_to_code = argv[1];
   read_program_code_into_memory(path_to_code);
 
-  /* 0x3000 is the default PC position, start of memory available for user programs. */
+  // 0x3000 is the default PC position, start of memory available for programs
   uint16_t PC_INIT = 0x3000;
   reg[R_PC] = PC_INIT;
 
   while (1)
   {
-    uint16_t instruction = read_from_memory(reg[R_PC]++); // load instruction from memory
+    // load instruction from memory
+    uint16_t instruction = read_from_memory(reg[R_PC]++);
     uint16_t opcode = instruction >> 12;
 
-    switch (opcode)
-    {
-    case OP_BR:
-      op_br(instruction);
-      break;
-    case OP_ADD:   /* add */
-      op_add(instruction);
-      break;
-    case OP_LD:     /* load */
-      op_ld(instruction);
-      break;
-    case OP_ST:     /* store */
-      op_st(instruction);
-      break;
-    case OP_JSR:    /* jump register */
-      op_jsr(instruction);
-      break;
-    case OP_AND:    /* bitwise and */
-      op_and(instruction);
-      break;
-    case OP_LDR:    /* load register */
-      op_ldr(instruction);
-      break;
-    case OP_STR:    /* store register */
-      op_str(instruction);
-      break;
-    case OP_RTI:    /* unused */
-      //op_rti(instruction);
-      break;
-    case OP_NOT:    /* bitwise not */
-      op_not(instruction);
-      break;
-    case OP_LDI:    /* load indirect */
-      op_ldi(instruction);
-      break;
-    case OP_STI:    /* store indirect */
-      op_sti(instruction);
-      break;
-    case OP_JMP:    /* jump */
-      op_jmp(instruction);
-      break;
-    case OP_RES:    /* reserved (unused) */
-      //op_res(instruction);
-      break;
-    case OP_LEA:   /* load effective address */
-      op_lea(instruction);
-      break;
-    case OP_TRAP:    /* execute trap */
-      op_trap(instruction);
-      break;
-    default:
-      abort();
-      break;
+    switch (opcode) {
+      
+      // branch
+      case OP_BR:
+        op_br(instruction);
+        break;
+      
+      // add
+      case OP_ADD:
+        op_add(instruction);
+        break;
+      
+      // load
+      case OP_LD:
+        op_ld(instruction);
+        break;
+      
+      // store
+      case OP_ST:
+        op_st(instruction);
+        break;
+      
+      // jump register
+      case OP_JSR:
+        op_jsr(instruction);
+        break;
+
+      // bitwise and
+      case OP_AND:
+        op_and(instruction);
+        break;
+
+      // load register
+      case OP_LDR:
+        op_ldr(instruction);
+        break;
+      
+      // store register
+      case OP_STR:
+        op_str(instruction);
+        break;
+
+      // unused
+      case OP_RTI:
+        break;
+
+      // bitwise not
+      case OP_NOT:
+        op_not(instruction);
+        break;
+
+      // load indirect
+      case OP_LDI:
+        op_ldi(instruction);
+        break;
+      
+      // store indirect
+      case OP_STI:
+        op_sti(instruction);
+        break;
+      
+      // jump
+      case OP_JMP:
+        op_jmp(instruction);
+        break;
+      
+      // reserve (unused)
+      case OP_RES:
+        break;
+
+      // load effective address  
+      case OP_LEA:
+        op_lea(instruction);
+        break;
+      
+      // execute trap
+      case OP_TRAP:
+        op_trap(instruction);
+        break;
+      
+      default:
+        abort();
+        break;
     }
   }
 
--- opcode.c
+++ opcode.c
@@ -12,234 +12,334 @@ enum trap_codes
   T_HALT
 };
 
-/* Opcode Functions */
 
-
-/* Note: to break bits into sections
-*  - use mask of 1s of size of target section to only keep bits of target section (remove bits left of target section)
-*  - use bit-shift to erase bits right of target section
+/*
+* Opcode Functions
+-----------------------------
+* Naming conventions follow the conventions used by the descriptions of opcodes
+* written by the creators of the LC-3 project found here: 
+* http://highered.mheducation.com/sites/dl/free/0072467509/104652/LC3_unix.pdf
+*
+* Args:
+*   uint16_t bits: binary number where the first digit indicates sign, the 
+*     next four digits indicate opcode to execute, and the remaining digits 
+*     represent other information used during execution
+*
+* Standardized variables (all uint16_t unless otherwise noted):
+*   DR: destination register; where to store content
+*   SR, SR1: source register 1; location of content
+*   SR2: source register 2; location of content
+*   BaseR: base register used to compute Base+offset addresses
+*   imm5: 5-bit immediate value; bits [4:0] of an instruction
+*   offset6: 6-bit value; bits [5:0] of an instruction
+*   PCoffsetN: bits [N-1:0] of an instruction taken as an N-bit 2’s 
+*     complement integer, sign-extended to 16 bits, and added to the 
+*     incremented PC
 */
 
-// ✅
 void op_add(uint16_t bits)
- {
-   /* destination register (DR) */
-   uint16_t DR = (bits >> 9) & 0x7;
-   /* first source register (SR1) */
-   uint16_t SR1 = (bits >> 6) & 0x7;
-   /* check flag source register for mode of ADD */
-   if ((bits >> 5) & 0x1)
-   {
-     /* mode 1: add a small constant */
-     uint16_t imm5 = get_sign_extension(bits & 0b11111, 5);
-     reg[DR] = reg[SR1] + imm5;
-   }
-   else
-   {
-     /* mode 2: add value from second source register */
-     uint16_t SR2 = (bits >> 0) & 0x7;
-     reg[DR] = reg[SR1] + reg[SR2];
-   }
-  //  update_flag(reg[DR]);
-   update_flag(DR);
- }
-
-// ✅
-void op_and(uint16_t bits)
- {
-   /* destination register (DR) */
-   uint16_t DR = (bits >> 9) & 0x7;
-   /* first source register (SR1) */
-   uint16_t SR1 = (bits >> 6) & 0x7;
-   /* check flag source register for mode */
-   if ((bits >> 5) & 0x1)
-   {
-     /* mode 1: and a small constant */
-     uint16_t imm5 = get_sign_extension(bits & 0x1F, 5);
-     reg[DR] = reg[SR1] & imm5;
-   }
-   else
-   {
-     /* mode 2: and value from second source register (SR2) */
-     uint16_t SR2 = bits & 0x7;
-     reg[DR] = reg[SR1] & reg[SR2];
-   }
-  //  update_flag(reg[DR]);
-   update_flag(DR);
- }
-
-// ✅
-void op_not(uint16_t bits)
 {
-  /* destination register (DR) */
+  /*
+  * The second source operand is added to the contents of SR1 and the result is
+  * stored in DR. There are two modes indicated by bit [5]: if bit [5] = 1, the
+  * second source operand is obtained from imm5; if bit [5] = 0, the second 
+  * source operand is obtained from SR2. Condition codes are set based on 
+  * whether the result is negative, zero, or positive.
+  */
+
   uint16_t DR = (bits >> 9) & 0x7;
-  /* first source register (SR1) */
   uint16_t SR1 = (bits >> 6) & 0x7;
-  reg[DR] = ~reg[SR1];
+  if ((bits >> 5) & 0x1) {
+    // mode 1: bit[5]=1, add a small constant
+    uint16_t imm5 = get_sign_extension(bits & 0b11111, 5);
+    reg[DR] = reg[SR1] + imm5;
+  }
+  else {
+    // mode 2: bit[5]=0, add value from second source register
+    uint16_t SR2 = (bits >> 0) & 0x7;
+    reg[DR] = reg[SR1] + reg[SR2];
+  }
   update_flag(DR);
 }
 
-// ✅
+void op_and(uint16_t bits)
+{
+  /*
+  * The second source operand is ANDed with the contents of SR1 and the result 
+  * is stored in DR. Like op_add, there are two modes indicated by bit [5]: if 
+  * bit [5] = 1, the second source operand is obtained from imm5; if bit [5] =
+  * 0, the second source operand is obtained from SR2. Condition codes are set 
+  * based on whether the result is negative, zero, or positive.
+  */
+
+  uint16_t DR = (bits >> 9) & 0x7;
+  uint16_t SR1 = (bits >> 6) & 0x7;
+  // check flag source register for mode
+  if ((bits >> 5) & 0x1) {
+    // mode 1: and a small constant
+    uint16_t imm5 = get_sign_extension(bits & 0x1F, 5);
+    reg[DR] = reg[SR1] & imm5;
+  }
+  else {
+    // mode 2: and value from SR2
+    uint16_t SR2 = bits & 0x7;
+    reg[DR] = reg[SR1] & reg[SR2];
+  }
+  update_flag(DR);
+}
+
+void op_not(uint16_t bits)
+{
+  /*
+  * The bit-wise complement of the contents of SR is stored in DR. Condition 
+  * codes are set based on whether the result is negative, zero, or positive.
+  */
+
+  uint16_t DR = (bits >> 9) & 0x7;
+  uint16_t SR = (bits >> 6) & 0x7;
+  reg[DR] = ~reg[SR];
+  update_flag(DR);
+}
+
 void op_ld(uint16_t bits)
 {
-  /* destination register (DR) */
+  /*
+  * Address computed by sign-extending bits [8:0] to 16 bits and adding 
+  * this value to the incremented PC. The contents of memory at this address 
+  * are loaded into DR. Condition codes are set based on whether the value 
+  * loaded is negative, zero, or positive.
+  */
+
   uint16_t DR = (bits >> 9) & 0x7;
-  // uint16_t PCoffset9 = bits & 0x1FF;
   uint16_t PCoffset9 = bits;
   uint16_t address = reg[R_PC] + get_sign_extension(PCoffset9, 9);
   reg[DR] = read_from_memory(address);
   update_flag(DR);
-
-  /* debug implmenetation, only use if op_ld is sad */
-  // uint16_t R_0 = (bits >> 9) & 0x7;
-  // uint16_t pc_offset = get_sign_extension(bits & 0x1FF, 9);
-  // reg[R_0] = read_from_memory(reg[R_PC] + pc_offset);
-  // update_flag(R_0);
 }
 
-// ✅
 void op_ldi(uint16_t bits)
 {
-   /* destination register (DR) */
+  /*
+  * Address computed by sign-extending bits [8:0] to 16 bits and adding 
+  * this value to the incremented PC. The address of the data to be loaded into
+  * DR is stored in memory at this address. Condition codes are set based on 
+  * whether the value loaded is negative, zero, or positive.
+  */
+
   uint16_t DR = (bits >> 9) & 0x7;
-  /* PCoffset 9*/
   uint16_t PCoffset9 = get_sign_extension(bits & 0x1FF, 9);
-  /* add pc_offset to the current PC, look at that memory location to get the final address */
   reg[DR] = read_from_memory(read_from_memory(reg[R_PC] + PCoffset9));
   update_flag(DR);
 }
 
-// ✅
 void op_ldr(uint16_t bits)
 {
-  /* destination register (DR) */
+  /*
+   * Address computed by sign-extending bits [5:0] to 16 bits and adding 
+   * this value to the contents of the register specified in bits [8:6]. The 
+   * contents of memory at this address are loaded into DR. Condition codes are
+   * set based on whether the value loaded is negative, zero, or positive.
+   */
+
   uint16_t DR = (bits >> 9) & 0x7;
   uint16_t BaseR = (bits >> 6) & 0x7;
   uint16_t offset6 = bits & 0x3f;
   uint16_t address = reg[BaseR] + get_sign_extension(offset6, 6);
   reg[DR] = read_from_memory(address);
   update_flag(DR);
-
-  /* old implementation, only use if it becomes sad */
-  // uint16_t R_0 = (bits >> 9) & 0x7;
-  // uint16_t BaseR = (bits >> 6) & 0x7;
-  // uint16_t offset = get_sign_extension(bits & 0x3F, 6);
-  // reg[r0] = read_from_memory(reg[BaseR] + offset);
-  // update_flag(R_0);
 }
 
-// ✅
 void op_lea(uint16_t bits)
 {
-  /* destination register (DR) */
+  /*
+  * Address computed by sign-extending bits [8:0] to 16 bits and adding this 
+  * value to the incremented PC. This address is loaded into DR. Condition 
+  * codes are set based on whether the value loaded is negative, zero, or 
+  * positive.
+  */
+
   uint16_t DR = (bits >> 9) & 0x7;
   uint16_t PCoffset9 = bits & 0x1FF;
   uint16_t address = reg[R_PC] + get_sign_extension(PCoffset9, 9);
   reg[DR] = address;
   update_flag(DR);
-
-  /* old implementation, only use if it becomes sad */
-  // uint16_t R_0 = (bits >> 9) & 0x7;
-  // uint16_t pc_offset = get_sign_extension(bits & 0x1FF, 9);
-  // reg[R_0] = reg[R_PC] + pc_offset;
-  // update_flag(R_0);
 }
 
-// ✅
+void op_br(uint16_t bits)
+{
+  /*
+  * The condition codes specified by bits [11:9] are tested. If bit [11] is 
+  * set, N is tested; if bit [11] is clear, N is not tested, etc. If any 
+  * condition code tested is set, the program branches to the location 
+  * specified by adding the sign-extended PCoffset9 to the incremented PC.
+  */
+
+  uint16_t PCoffset9 = get_sign_extension(bits & 0x1FF, 9);
+  uint16_t cond_flag = (bits >> 9) & 0x7;
+  if (cond_flag & reg[R_F]) {
+    reg[R_PC] += PCoffset9;
+  }
+}
+
+void op_jmp(uint16_t bits)
+{
+  /* 
+  * Program unconditionally jumps to the location specified in BaseR, which 
+  * is given by bits [8:6]. Although standard implementations of LC-3 use RET, 
+  * RET is omitted here in favor of simplicity.
+  */
+
+  uint16_t BaseR = (bits >> 6) & 0x7;
+  // adjust PC to move to new address
+  reg[R_PC] = reg[BaseR]; 
+}
+
+void op_jsr(uint16_t bits)
+{
+  /* 
+  * Jumps to new address and links previous location. The incremented PC is 
+  * saved as temp. Then PC is loaded with the address of the first instruction 
+  * of the subroutine, causing an unconditional jump to that address. The 
+  * address of the subroutine is obtained from BaseR (if bit [11] is 0), or 
+  * the address is computed by sign-extending bits [10:0] and adding it to the 
+  * incremented PC (if bit [11] is 1). Finally, R7 is loaded with the value 
+  * in temp. This is the link back to the calling routine.
+  */
+
+  // assign PC to temp register for backlinking
+  uint16_t temp = reg[R_PC];
+  // check if opcode indicates jsr or jsrr
+  uint16_t jsr_flag = (bits >> 11) & 1;
+  if (jsr_flag) {
+      // run jsr taking PCoffset11
+      uint16_t PCoffset11 = get_sign_extension(bits & 0x7FF, 11);
+      // jump via given PCoffset11
+      reg[R_PC] += PCoffset11;
+    }
+  else {
+    // run jsrr taking BaseR
+    uint16_t BaseR = (bits >> 6) & 0x7;
+    // jump to register val
+    reg[R_PC] = reg[BaseR]; 
+  }
+  reg[R_7] = temp;
+}
+
+void op_st(uint16_t bits)
+{
+  /*
+  * Stores the contents of the register specified in SR at a location in 
+  * memory. The address of this location is found by sign-extending bits [8:0] 
+  * to 16 bits and adding it to the incremented PC.
+  */
+
+  uint16_t SR = (bits >> 9) & 0x7;
+  uint16_t PCoffset9 = get_sign_extension(bits, 9);
+  uint16_t address = (reg[R_PC] + PCoffset9);
+  write_to_memory(address, reg[SR]);
+}
+
+void op_sti(uint16_t bits)
+{
+  /*
+  * Stores the contents of the register specified in SR at a location in 
+  * memory. The address of this location is obtained as follows: bits [8:0] 
+  * are sign-extended to 16 bits and added to the incremented PC. This memory 
+  * address contains the address of the location to which the data in SR is 
+  * stored.
+  */
+
+  uint16_t SR = (bits >> 9) & 0x7;
+  uint16_t PCoffset9 = get_sign_extension(bits, 9);
+  uint16_t address = (reg[R_PC] + PCoffset9);
+  write_to_memory(read_from_memory(address), reg[SR]);
+}
+
+void op_str(uint16_t bits)
+{
+  /*
+  * Stores the contents of the register specified in SR at a location in 
+  * memory. The address of this location is computed by sign-extending 
+  * bits [5:0] to 16 bits and adding it to the contents of the register 
+  * specified in bits [8:6].
+  */
+
+  uint16_t SR1 = (bits >> 9) & 0x7;
+  uint16_t BaseR = (bits >> 6) & 0x7;
+  uint16_t PCoffset6 = get_sign_extension(bits & 0x3F, 6);
+  uint16_t address = reg[BaseR] + PCoffset6;
+  write_to_memory(address, reg[SR1]);
+}
+
 void trap_getc()
 {
-  /**
-   * Read a single character from the keyboard.
-   * The character is not echoed onto theconsole.
-   * Its ASCII code is copied into R0.
-   * The high eight bits of R0 are cleared.
-   */
+  /*
+  * Reads a character from keyboard.
+  * The character's ASCII code is copied to R_0; then the first 8 bits of R_0 
+  * are cleared.
+  */
+
   reg[R_0] = getchar() & 0b11111111;
   fflush(stdin);
-
-  /* old implementation, only use if trap_getc becomes sad */
-  // reg[R_0] = (uint16_t)getchar();
 }
 
-// ✅
 void trap_out()
 {
-  /**
-   * Write a character in R0[7:0] to the console display.
-   * Avoid automatic buffering
-   * Clear (or flush) the output buffer and move the buffered data to console
-   */
+  /*
+  * Writes a character in R_0 to console avoiding automatic buffering. 
+  * Clears (or flushes) output buffer and prints buffered data to console. 
+  */
+
   fprintf(stdout, "%c", reg[R_0]);
   fflush(stdout);
-
-  /* debug implementation, only use if trap_out gets problems somehow */
-  // putc((char)reg[R_0], stdout);
-  // fflush(stdout);
 }
 
-// ✅
 void trap_in()
 {
-  /**
-   * Print a prompt on the screen and read a single character from the keyboard.
-   * The character is echoed onto the console monitor, and its ASCII code is copied into R0.
-   * The high eight bits of R0 are cleared.
-   */
+  /*
+  * Prints a prompt and reads a character from keyboard. The character is 
+  * echoed to the console and its ASCII code is copied to R_0, then 
+  * the first eight bits of R_0 are cleared.
+  */
+
   puts("Enter a character:\n");
   reg[R_0] = getchar() & 0b11111111;
   fprintf(stdout, "%c", reg[R_0]);
   fflush(stdin);
   fflush(stdout);
-
-  /* debug implementation, only use if it becomes a problem child*/
-  // printf("Enter a character: ");
-  // char c = getchar();
-  // putc(c, stdout);
-  // reg[R_0] = (uint16_t)c;
 }
 
-// ✅
 void trap_puts()
 {
-  /**
-   * Write a string of ASCII characters to the console.
-   * The characters are contained in consecutive memory locations, two characters per memory location, starting with the address specified in R0.
-   * The ASCII code contained in bits [7:0] of a memory location is written to the console first.
-   * Then the ASCII code contained in bits [15:8] of that memory location is written to the console.
-   * (A character string consisting of an odd number of characters to be written will have x00 in bits [15:8] of the memory location containing the last character to be written.)
-   * Writing terminates with the occurrence of x0000 in a memory location.
-   */
+  /*
+  * Writes a string of ASCII characters to the console beginning at the address
+  * specified in R_0 and continuing with consecutive addresses until x0000 
+  * occurs.
+  */
 
-  uint16_t start = reg[R_0];
-  uint16_t val = read_from_memory(start);
-  while (val)
-  {
-    // QUESTION: do we have to convert back to big endian
+  uint16_t address = reg[R_0];
+  uint16_t val = read_from_memory(address);
+  while (val) {
     char first_char = val & 0xFF;
     putc(first_char, stdout);
     char second_char = val >> 8;
+    // if second char exists, write to stdout
     if (second_char) {
-      // if second char exists
       putc(first_char, stdout);
     }
-    val = read_from_memory(++start);
+    val = read_from_memory(++address);
   }
-  fflush(stdout); // move the buffered data to console
-
-  /* oldish debugging implementation, replace if it causes problems */
-  // uint16_t* c = memory + reg[R_0];
-  // while (*c)
-  // {
-  //     putc((char)*c, stdout);
-  //     ++c;
-  // }
-  // fflush(stdout);
-
+  // move buffered data to console
+  fflush(stdout); 
 }
 
 void trap_putsp()
 {
-  uint16_t* c = memory + reg[R_0];
+  /* 
+  * Prints the contents of R_0 to console.
+  */
+
+  uint16_t *c = memory + reg[R_0];
   while (*c)
   {
     char char1 = (*c) & 0xFF;
@@ -251,28 +351,30 @@ void trap_putsp()
     ++c;
   }
   fflush(stdout);
-
 }
 
-// ✅
 void trap_halt()
 {
-  /** Halt execution and print a message on the console. */
+  /*
+  * Halts execution and prints a message to the console.
+  */
+
   puts("\nHALT\n");
   fflush(stdout);
   restore_input_buffering();
   exit(1);
 }
 
-// ✅
 void op_trap(uint16_t bits)
 {
-  /* First R7 is loaded with the incremented PC. (This enables a return to the uction
-physically following the TRAP uction in the original program after the service
-routine has completed execution.) */
+  /*
+  * R_7 is loaded with the incremented PC. (This enables a return to the 
+  * function following the TRAP function in the original program after the 
+  * service routine has executed).
+  */
+
   uint16_t trapvector8 = bits & 0b11111111;
   switch (trapvector8)
-  //switch (bits & 0xFF)
   {
   case T_GETC:
     trap_getc();
@@ -296,80 +398,4 @@ routine has completed execution.) */
     abort();
     break;
   }
-}
-
-// ✅
-void op_br(uint16_t bits)
-{
-  uint16_t pc_offset = get_sign_extension(bits & 0x1FF, 9);
-  uint16_t cond_flag = (bits >> 9) & 0x7;
-  if (cond_flag & reg[R_F])
-  {
-      reg[R_PC] += pc_offset;
-  }
-}
-
-// ✅
-void op_jmp(uint16_t bits)
-{
-  /* Jumping into a new address! :D */
-  uint16_t r1 = (bits >> 6) & 0x7; // putting into unsigned 16 bit int
-  reg[R_PC] = reg[r1]; // adjust program counter to move to the new place
-}
-
-// ✅
-void op_jsr(uint16_t bits)
-{
-  /* Jumping into a new address and linking back prev location */
-
-  // assigning program counter to temp register so we can link back
-  uint16_t temp = reg[R_PC];
-  //reg[R_7] = reg[R_PC];
-
-  // checking if opcode indicates jsr or jsrr
-  uint16_t jsr_flag = (bits >> 11) & 1;
-  if (jsr_flag) {
-      // we run jsr, taking a PCoffset
-      uint16_t pc_offset = get_sign_extension(bits & 0x7FF, 11);
-      reg[R_PC] += pc_offset;  // jump via offset given
-    }
-
-  else {
-    // We run jsrr, taking BaseR
-    uint16_t r1 = (bits >> 6) & 0x7;
-    reg[R_PC] = reg[r1]; // jump to register val
-  }
-
-  reg[R_7] = temp;
-  // originally was commented out (as was temp = reg[R_PC])
-}
- // ✅
-void op_st(uint16_t bits)
-{
-  uint16_t SR = (bits >> 9) & 0x7;
-  uint16_t PCoffset9 = get_sign_extension(bits, 9);
-  // uint16_t pc_offset = get_sign_extension(bits & 0x1FF, 9);
-  uint16_t address = (reg[R_PC] + PCoffset9);
-  write_to_memory(address, reg[SR]);
-}
-
- // ✅
-void op_sti(uint16_t bits)
-{
-  uint16_t SR = (bits >> 9) & 0x7;
-  //uint16_t PCoffset9 = get_sign_extension(bits & 0x1FF, 9);
-  uint16_t PCoffset9 = get_sign_extension(bits, 9);
-  uint16_t address = (reg[R_PC] + PCoffset9);
-  write_to_memory(read_from_memory(address), reg[SR]);
-}
-
- // ✅
-void op_str(uint16_t bits)
-{
-  uint16_t SR = (bits >> 9) & 0x7;
-  uint16_t baseR = (bits >> 6) & 0x7;
-  uint16_t PCoffset6 = get_sign_extension(bits & 0x3F, 6);
-  uint16_t address = reg[baseR] + PCoffset6;
-  write_to_memory(address, reg[SR]);
-
 }
